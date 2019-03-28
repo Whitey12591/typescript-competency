143deee68ec91bbff604476865fbb524b602daeb
Whitey12591 <danwhite1022@gmail.com>
2019-03-28 15:58:59 -0500

    generics

--- generics.ts
+++ generics.ts
@@ -8,4 +8,20 @@ const string1 = new BallOfString();
 
 
 
+class Toys<T> {
+  collection: T[] = [];
 
+  addItem(newItem: T) {
+    this.collection.push(newItem)
+  }
+}
+
+const dogToys = new Toys<Ball>();
+dogToys.addItem(ball1);
+//this next line shouldn't work
+//dogToys.addItem(string1);
+
+const catToys = new Toys<BallOfString>();
+catToys.addItem(string1);
+//this next line shouldn't work
+//catToys.addItem(ball1);
