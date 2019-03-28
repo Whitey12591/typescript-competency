8be86d44fa416da6de009df80741e785c5260c3d
Whitey12591 <danwhite1022@gmail.com>
2019-03-28 15:53:05 -0500

    accessor

--- accessor.ts
+++ accessor.ts
@@ -3,16 +3,18 @@ const appDiv2: HTMLElement = document.getElementById('app2');
 
 // exercise 1
 class Cat {
-  constructor() {}
+  constructor(public hairball: boolean) {
+
+  }
 
   meow() {
-    // if(this.hairball)
-    //   return 'meow - cough, cough'
-    // else
-    //   return 'meow '
+    if(this.hairball)
+      return 'meow - cough, cough'
+    else
+      return 'meow '
   }
 }
 
-// let kitty = new Cat(false)
-// kitty.hairball = true;
-// appDiv2.innerHTML = kitty.meow()
+let kitty = new Cat(false)
+kitty.hairball = true;
+appDiv2.innerHTML = kitty.meow()
